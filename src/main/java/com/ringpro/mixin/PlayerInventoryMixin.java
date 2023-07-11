f0e597d15e606b8dbba47a548e7a10deebd342aa
ringprod <lawsonherger@gmail.com>
2023-07-11 14:54:45 -0500

    fixed redirect incompatibility

--- src/main/java/com/ringpro/mixin/PlayerInventoryMixin.java
+++ src/main/java/com/ringpro/mixin/PlayerInventoryMixin.java
@@ -3,36 +3,51 @@ package com.ringpro.mixin;
 import com.ringpro.EasyDeathsMod;
 import com.ringpro.access.ItemEntityInterface;
 import net.minecraft.entity.ItemEntity;
-import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.collection.DefaultedList;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.List;
 
 @Mixin(PlayerInventory.class)
 public class PlayerInventoryMixin {
-    @Redirect(method = "dropAll",
-            at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/player/PlayerEntity;dropItem(Lnet/minecraft/item/ItemStack;ZZ)Lnet/minecraft/entity/ItemEntity;"))
-    private ItemEntity droppedItem(PlayerEntity player, ItemStack stack, boolean throwRandomly, boolean retainOwnership) {
-        ItemEntity itemEntity = player.dropItem(stack, throwRandomly, retainOwnership);
+    @Final @Shadow private List<DefaultedList<ItemStack>> combinedInventory;
 
-        if (itemEntity == null)
-            return null;
+    @Inject(method = "dropAll", at = @At(value = "HEAD"), cancellable = true)
+    private void dropAllInject(CallbackInfo ci) {
 
-        boolean isInvulnerable = itemEntity.getEntityWorld().getGameRules().get(EasyDeathsMod.DEATH_ITEMS_INVULNERABLE).get();
-        boolean itemNeverDespawn = itemEntity.getEntityWorld().getGameRules().get(EasyDeathsMod.DEATH_ITEMS_NEVER_DESPAWN).get();
-        boolean itemShouldGlow = itemEntity.getEntityWorld().getGameRules().get(EasyDeathsMod.DEATH_ITEMS_GLOW).get();
+        for (List<ItemStack> list : combinedInventory) {
+            for (int i = 0; i < list.size(); ++i) {
+                ItemStack itemStack = list.get(i);
+                if (!itemStack.isEmpty()) {
+                    ItemEntity itemEntity = ((PlayerInventory) (Object) this).player.dropItem(itemStack, true, false);
+                    list.set(i, ItemStack.EMPTY);
 
-        if (itemNeverDespawn) {
-            itemEntity.setNeverDespawn();
+                    if (itemEntity != null) {
+                        boolean isInvulnerable = itemEntity.getEntityWorld().getGameRules().get(EasyDeathsMod.DEATH_ITEMS_INVULNERABLE).get();
+                        boolean itemNeverDespawn = itemEntity.getEntityWorld().getGameRules().get(EasyDeathsMod.DEATH_ITEMS_NEVER_DESPAWN).get();
+                        boolean itemShouldGlow = itemEntity.getEntityWorld().getGameRules().get(EasyDeathsMod.DEATH_ITEMS_GLOW).get();
+
+                        if (itemNeverDespawn) {
+                            itemEntity.setNeverDespawn();
+                        }
+                        if (isInvulnerable) {
+                            ((ItemEntityInterface) itemEntity).setAcutallyInvulnerable(true);
+                        }
+                        if (itemShouldGlow) {
+                            ((ItemEntityInterface) itemEntity).setShouldGlow(true);
+                        }
+                    }
+                }
+            }
         }
-        if (isInvulnerable) {
-            ((ItemEntityInterface) itemEntity).setAcutallyInvulnerable(true);
-        }
-        if (itemShouldGlow) {
-            ((ItemEntityInterface) itemEntity).setShouldGlow(true);
-        }
-        return itemEntity;
+        ci.cancel();
     }
+
 }
